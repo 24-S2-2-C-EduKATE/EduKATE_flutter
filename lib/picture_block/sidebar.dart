47e9496737915e7f47866a1bcf14884464f36045
Wenhui <u7773637@anu.edu.au>
2024-10-03 01:36:25 +1000

    remove background

--- lib/picture_block/sidebar.dart
+++ lib/picture_block/sidebar.dart
@@ -16,99 +16,91 @@ class Sidebar extends StatelessWidget {
         child: isOpen
             ? Consumer<VirtualController>(
                 builder: (context, virtualController, child) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/sidebar_background.jpg'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            'Current Level: ${virtualController.currentLevel?.id ?? 'N/A'}',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Current Level: ${virtualController.currentLevel?.id ?? 'N/A'}',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
-                        Expanded(
-                          child: virtualController.isLoading
-                              ? Center(child: CircularProgressIndicator())
-                              : Column(
-                                  children: [
-                                    Expanded(
-                                      child: AspectRatio(
-                                        aspectRatio: 1,
-                                        child: GridView.builder(
-                                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                            crossAxisCount: virtualController.currentLevel!.gridN,
-                                          ),
-                                          itemCount: virtualController.activeGrid.length,
-                                          itemBuilder: (context, index) {
-                                            return Container(
-                                              decoration: BoxDecoration(
-                                                border: Border.all(color: const Color.fromARGB(255, 238, 213, 113)),
-                                              ),
-                                              child: Image.asset(
-                                                'assets/blocks/${virtualController.activeGrid[index].tileType}.png',
-                                                fit: BoxFit.fill,
-                                              ),
-                                            );
-                                          },
+                      ),
+                      Expanded(
+                        child: virtualController.isLoading
+                            ? Center(child: CircularProgressIndicator())
+                            : Column(
+                                children: [
+                                  Expanded(
+                                    child: AspectRatio(
+                                      aspectRatio: 1,
+                                      child: GridView.builder(
+                                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount: virtualController.currentLevel!.gridN,
                                         ),
+                                        itemCount: virtualController.activeGrid.length,
+                                        itemBuilder: (context, index) {
+                                          return Container(
+                                            decoration: BoxDecoration(
+                                              border: Border.all(color: const Color.fromARGB(255, 238, 213, 113)),
+                                            ),
+                                            child: Image.asset(
+                                              'assets/blocks/${virtualController.activeGrid[index].tileType}.png',
+                                              fit: BoxFit.fill,
+                                            ),
+                                          );
+                                        },
                                       ),
                                     ),
-                                  ],
-                                ),
-                        ),
-                        SizedBox(height: 50), // Negative space to move buttons up
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 50.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SizedBox(
-                                height: 40,
-                                width: 125,
-                                child: ElevatedButton(
-                                  onPressed: virtualController.previousLevel,
-                                  child: const Text('Previous Level', style: TextStyle(fontSize: 13)),
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: const Color.fromARGB(255, 231, 152, 178),
-                                    foregroundColor: Colors.white,
-                                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
+                                  ),
+                                ],
+                              ),
+                      ),
+                      SizedBox(height: 50), // Negative space to move buttons up
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            SizedBox(
+                              height: 40,
+                              width: 125,
+                              child: ElevatedButton(
+                                onPressed: virtualController.previousLevel,
+                                child: const Text('Previous Level', style: TextStyle(fontSize: 13)),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color.fromARGB(255, 231, 152, 178),
+                                  foregroundColor: Colors.white,
+                                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
                                   ),
                                 ),
                               ),
-                              SizedBox(
-                                height: 40,
-                                width: 125, 
-                                child: ElevatedButton(
-                                  onPressed: virtualController.nextLevel,
-                                  child: const Text('Next Level', style: TextStyle(fontSize: 13)),
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: const Color.fromARGB(255, 231, 152, 178),
-                                    foregroundColor: Colors.white,
-                                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    ),
+                            ),
+                            SizedBox(
+                              height: 40,
+                              width: 125, 
+                              child: ElevatedButton(
+                                onPressed: virtualController.nextLevel,
+                                child: const Text('Next Level', style: TextStyle(fontSize: 13)),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color.fromARGB(255, 231, 152, 178),
+                                  foregroundColor: Colors.white,
+                                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   );
                 },
               )
