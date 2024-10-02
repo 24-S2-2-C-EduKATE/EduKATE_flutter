29497e8d92d9570c46c395a01b96cc5166845c2d
amyWenhui <u7773637@anu.edu.au>
2024-10-03 00:40:32 +1000

    add sidebar background and update the control position

--- lib/picture_block/sidebar.dart
+++ lib/picture_block/sidebar.dart
@@ -3,74 +3,116 @@ import 'package:flutter_application_1/picture_block/virtual_controller.dart';
 import 'package:provider/provider.dart';
 
 class Sidebar extends StatelessWidget {
-  final bool isOpen; // Boolean to check if the sidebar is open
-
-  Sidebar({required this.isOpen}); // Constructor to initialize the sidebar's open state
+  final bool isOpen;
+  
+  Sidebar({required this.isOpen});
 
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 300), // Duration for the sidebar's opening/closing animation
-      width: isOpen ? 300 : 0, // Width of the sidebar based on its open state
+      duration: Duration(milliseconds: 300),
+      width: isOpen ? 300 : 0,
       child: Drawer(
         child: isOpen
-            ? Consumer<VirtualController>(builder: (context, virtualController, child) {
-                return Column(
-                  children: [
-                    // Display the current level information
-                    Text('Current Level: ${virtualController.currentLevel?.id ?? 'N/A'}'),
-                    Expanded(
-                      child: virtualController.isLoading // Show loading indicator if data is loading
-                          ? Center(child: CircularProgressIndicator())
-                          : Column(
-                              children: [
-                                // Display the level ID
-                                Text('Level ${virtualController.currentLevel!.id}'),
-                                Expanded(
-                                  child: AspectRatio(
-                                    aspectRatio: 1, // Maintain a square aspect ratio for the grid
-                                    child: GridView.builder(
-                                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: virtualController.currentLevel!.gridN, // Number of columns in the grid
+            ? Consumer<VirtualController>(
+                builder: (context, virtualController, child) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/sidebar_background.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            'Current Level: ${virtualController.currentLevel?.id ?? 'N/A'}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: virtualController.isLoading
+                              ? Center(child: CircularProgressIndicator())
+                              : Column(
+                                  children: [
+                                    Expanded(
+                                      child: AspectRatio(
+                                        aspectRatio: 1,
+                                        child: GridView.builder(
+                                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: virtualController.currentLevel!.gridN,
+                                          ),
+                                          itemCount: virtualController.activeGrid.length,
+                                          itemBuilder: (context, index) {
+                                            return Container(
+                                              decoration: BoxDecoration(
+                                                border: Border.all(color: const Color.fromARGB(255, 238, 213, 113)),
+                                              ),
+                                              child: Image.asset(
+                                                'assets/blocks/${virtualController.activeGrid[index].tileType}.png',
+                                                fit: BoxFit.fill,
+                                              ),
+                                            );
+                                          },
+                                        ),
                                       ),
-                                      itemCount: virtualController.activeGrid.length, // Number of tiles in the grid
-                                      itemBuilder: (context, index) {
-                                        return Container(
-                                          decoration: BoxDecoration(
-                                            border: Border.all(), // Border for each tile
-                                          ),
-                                          child: Image.asset(
-                                            'assets/blocks/${virtualController.activeGrid[index].tileType}.png', // Load image based on tile type
-                                            fit: BoxFit.fill, // Fit image within the container
-                                          ),
-                                        );
-                                      },
+                                    ),
+                                  ],
+                                ),
+                        ),
+                        SizedBox(height: 50), // Negative space to move buttons up
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 50.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              SizedBox(
+                                height: 40,
+                                width: 125,
+                                child: ElevatedButton(
+                                  onPressed: virtualController.previousLevel,
+                                  child: const Text('Previous Level', style: TextStyle(fontSize: 13)),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color.fromARGB(255, 231, 152, 178),
+                                    foregroundColor: Colors.white,
+                                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
                                     ),
                                   ),
                                 ),
-                              ],
-                            ),
-                    ),
-                    // Row for level navigation buttons
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        // Button to go to the previous level
-                        ElevatedButton(
-                          onPressed: virtualController.previousLevel,
-                          child: const Text('Previous Level'),
-                        ),
-                        // Button to go to the next level
-                        ElevatedButton(
-                          onPressed: virtualController.nextLevel,
-                          child: const Text('Next Level'),
+                              ),
+                              SizedBox(
+                                height: 40,
+                                width: 125, 
+                                child: ElevatedButton(
+                                  onPressed: virtualController.nextLevel,
+                                  child: const Text('Next Level', style: TextStyle(fontSize: 13)),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color.fromARGB(255, 231, 152, 178),
+                                    foregroundColor: Colors.white,
+                                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                  ],
-                );
-              })
-            : null, // If sidebar is closed, return null
+                  );
+                },
+              )
+            : null,
       ),
     );
   }
